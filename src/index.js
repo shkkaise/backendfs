205f78aef9c3f11248c0d5c644ca243ff7ff5378
shkkaise <123665233+shkkaise@users.noreply.github.com>
2024-12-10 00:18:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4500;
 
 app.use(express.json());
 app.use(cors());
